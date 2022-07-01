da0e01dae3a5860ba548cc39ce17cc0625d728f2
Sachin patel <psachin375@gmail.com>
2022-07-01 12:37:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-
+import Home from "./Home";
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+    <Home/>
     </div>
   );
 }
