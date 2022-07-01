5a9d421aa971e658a115900d46f4565450ef8add
Sachin patel <psachin375@gmail.com>
2022-07-01 14:17:26 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
-function Home(){
-  
-  return(
-  <div>
-    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-    <button onClick() ="click" id="click">Click</button>
-    
-    
+import React, { useState } from 'react'
+function Home() {
+  const [data, setData] = useState("");
+  const shoot = () => {
+    setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
+  return (
+    <div>
+    <p>{data}</p>
+      <button onClick={shoot}>Take the Shot!</button>
     </div>
   )
-  
 }
+
 export default Home;
