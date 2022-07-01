c473004ebf248e8b6ce7f10c44ec159f24cf559e
Sachin patel <psachin375@gmail.com>
2022-07-01 14:25:54 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 function Home() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState();
   const shoot = () => {
     setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
   }
