6fecce93d28831359edffc4d68c427443503a39d
Sachin patel <psachin375@gmail.com>
2022-07-01 12:41:49 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+function Home(){
+  
+  return(
+  <div>
+    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    <button onClick() ="click" id="click">Click</button>
+    
+    
+    </div>
+  )
+  
+}
+export default Home;
