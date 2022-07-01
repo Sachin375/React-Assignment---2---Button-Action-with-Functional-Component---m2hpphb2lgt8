5bad382ab0c2c805f8f0a1b2e513380bfa40a768
Sachin patel <psachin375@gmail.com>
2022-07-01 14:52:17 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,13 @@ import React, { useState } from 'react'
 
 function Home() {
   const [data, setData] = useState();
-  const [val, setVal] = useState("hidden");
   const shoot = () => {
-
     setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-    setVal("visible");
   }
   return (
     <div>
-      <p id="para" style={{visibility:val}}>{data}</p>
-      <button id="click" onClick={shoot}>Take the Shot!</button>
+      <p id="para">{data}</p>
+      <button id="click" onClick={shoot}>click</button>
     </div>
   )
 }
