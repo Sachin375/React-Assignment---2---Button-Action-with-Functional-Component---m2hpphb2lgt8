12f79ac7117d76f4495c961b317562ea12bc3471
Sachin patel <psachin375@gmail.com>
2022-07-04 21:11:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 function App() {
   
-  const [data, setData] = useState();
+  const [data, setData] = useState("");
   const shoot = () => {
     setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
   }
